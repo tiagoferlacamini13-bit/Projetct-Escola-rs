d501c73fe05b5c7f41d5f75163158f182fc17787
Tiago <tiagoferlacamini13@gmail.com>
2026-06-10 22:19:49 -0300

    login into pages

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -1,15 +1,18 @@
 function pegarDados() {
+
     let nome = document.querySelector("#nome").value
     let email = document.querySelector("#email").value
     let senha = document.querySelector("#senha").value
     localStorage.setItem('usuario', JSON.stringify({ nome, email, senha }))
+    window.location.href = '/assets/pages/Comfirmation/comfirmation.html';
 }
 function soltarDados() {
     const usuario = JSON.parse(localStorage.getItem('usuario'))
 
     if (usuario) {
-        document.getElementById('nome').value = usuario.nome
-        document.getElementById('email').value = usuario.email
-        document.getElementById('senha').value = usuario.senhas
+        document.getElementById('nome').innerText = usuario.nome
+        document.getElementById('email').innerText = usuario.email
+        document.getElementById('senha').innerText = usuario.senha
+
     }
 }
