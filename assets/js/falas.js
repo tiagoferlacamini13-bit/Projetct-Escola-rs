dc46fece7a317b2e631b4273101737fdddf838d9
BLUEZINHO <gabrielalves14g@gmail.com>
2026-07-10 17:37:13 -0300

    arrumando a página inicial

--- assets/js/falas.js
+++
@@ -1,7 +0,0 @@
-let caixa = document.createElement("textarea");
-
-caixa.placeholder = "Digite algo aqui";
-caixa.style.width = "1000px";
-caixa.style.height = "200px";
-
-document.getElementById("container").appendChild(caixa);
